6fe205b79adda1a8d2219e724ab06e987e6e002c
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-10-11 00:20:26 +0800

    Create files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, {useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import Logo from '../assets/logo.png'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav(!nav)
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="Logo" style={{width: '100px'}} />
+        </div>
+                   
+       {/*pc screen*/}
+           <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul> 
+       
+
+        <div onClick={handleClick} className='md:hidden z-10'>
+            <FaBars />
+        </div>
+
+      {/*mobile screen*/}
+           <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li classname='py-6 text-4xl'>Home</li>
+                <li classname='py-6 text-4xl'>Skills</li>
+                <li classname='py-6 text-4xl'>Projects</li>
+                <li classname='py-6 text-4xl'>Contact</li>
+            </ul> 
+   
+
+
+    </div>
+  )
+}
+
+export default Navbar
