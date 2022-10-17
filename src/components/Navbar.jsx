92ee6b32806e828db3d071372a0ec5328b9bd871
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-10-17 23:38:26 +0800

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#fae5d4] text-black-300'>
         <div>
-            <img src={Logo} alt="Logo" style={{width: '100px'}} />
+            <img src={Logo} alt="Logo" style={{width: '300px'}} />
         </div>
                    
        {/*pc screen*/}
@@ -22,17 +22,27 @@ const Navbar = () => {
        
 
         <div onClick={handleClick} className='md:hidden z-10'>
-            <FaBars />
+            {!nav ? <FaBars /> : <FaTimes/>}
         </div>
 
       {/*mobile screen*/}
            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-                <li classname='py-6 text-4xl'>Home</li>
-                <li classname='py-6 text-4xl'>Skills</li>
-                <li classname='py-6 text-4xl'>Projects</li>
-                <li classname='py-6 text-4xl'>Contact</li>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Projects</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul> 
    
+      {/*Social icons*/}
+      <div className='flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <a href='/'>
+
+          </a>
+        </ul>
+      </div>
+
+
 
 
     </div>
