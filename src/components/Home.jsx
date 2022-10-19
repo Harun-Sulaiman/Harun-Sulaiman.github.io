7ef92e20b6eec1f8630e9bf2e1eb4042e27fe290
Harun-Sulaiman <114136224+Harun-Sulaiman@users.noreply.github.com>
2022-10-20 00:20:26 +0800

    Creating Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,21 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div>
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p>Hello, my name is</p>
+        <h1>Harun Sulaiman</h1>
+        <h2>I'm a Front End Developer.</h2>
+        <p>Goal-oriented Front End Developer
+            looking to enhance professional experience specializing in design
+            and development. Armed with a deep sense of responsibility and
+            possessing very high levels of enthusiasm to give 110% for any endeavor
+        </p>
+        <div>
+          <button>View Projects <HiArrowNarrowRight/></button>
+        </div>
+      </div>
 
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React, {useState } from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import Logo from '../assets/logo4.png'
+import React, {useState } from 'react';
+import { FaBars, FaTimes, FaGithub, FaFacebook } from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import Logo from '../assets/logo4.png';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -15,7 +17,7 @@ const Navbar = () => {
         </div>
                    
        {/*pc screen*/}
-           <ul className='hidden md:flex '>
+           <ul className='hidden md:flex mr-[150px]'>
                 <li>Home</li>
                 <li>Skills</li>
                 <li>Projects</li>
@@ -36,12 +38,37 @@ const Navbar = () => {
                 <li className='py-6 text-4xl'>Contact</li>
             </ul> 
    
-      {/*Social icons*/}
-      <div className='flex fixed flex-col top-[35%] left-0'>
+      {/*icons*/}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <a href='/'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 '>
+            <a className='flex justify-between items-center w-full text-gray-300' 
+            href='/'>
+              Facebook <FaFacebook size={30} />
+            </a>
+          </li>
 
-          </a>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333] '>
+            <a className='flex justify-between items-center w-full text-gray-300' 
+            href='/'>
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2a0c46] '>
+            <a className='flex justify-between items-center w-full text-gray-300' 
+            href='/'>
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#125055] '>
+            <a className='flex justify-between items-center w-full text-gray-300' 
+            href='/'>
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+
         </ul>
       </div>
 
